5097b7271bf56b0df6b726dbbdd717da5a3f61c0
阿部直也 <n.abe@gemcook.com>
2020-08-05 08:49:58 +0900

    Added useReducer

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,106 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { withCookies } from 'react-cookie';
+import axios from 'axios';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {
+  START_FETCH,
+  FETCH_SUCCESS,
+  ERROR_CATCHED,
+  INPUT_EDIT,
+  TOGGLE_MODE,
+} from './actionTypes';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  span: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'teal',
+    cursor: 'pointer',
+  },
+  spanError: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: 'fuchsia',
+    marginTop: 10,
+  },
+}));
+
+// useReduserを使ってSignin.jsのコンポーネントを管理する
+const initialState = {
+  isLoading: false,
+  isSigninView: true,
+  error: '',
+  credentialsSignin: {
+    username: '',
+    password: '',
+  },
+  credentialsSignup: {
+    username: '',
+    password: '',
+  },
+};
+
+// stateを変更するreducerを書く
+const signinReducer = (state, action) => {
+  switch (action.type) {
+    case START_FETCH:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case ERROR_CATCHED:
+      return {
+        ...state,
+        error: 'Email or password is not correct',
+      };
+    case INPUT_EDIT:
+      return {
+        ...state,
+        [action.inputName]: action.payload,
+      };
+    case TOGGLE_MODE:
+      return {
+        ...state,
+        isSigninView: !state.isSigninView,
+      };
+    default:
+      return state;
+  }
+};
 
 const Signin = () => {
+  const classes = useStyles();
   return (
     <div>
       <p>Signin</p>

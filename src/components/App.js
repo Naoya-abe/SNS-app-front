686a2643704c997619ebfa6f8802f497e921222f
阿部直也 <n.abe@gemcook.com>
2020-08-04 10:02:19 +0900

    Added material-ui theme.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1565c0',
+    },
+    secondary: {
+      main: '#00897b',
+    },
+    typography: {
+      fontFamily: 'Roboto',
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       <p>App</p>
-    </div>
+    </MuiThemeProvider>
   );
 };
 

ff91d2eed122d041fd0ea6aaf09c011ee29774cb
阿部直也 <n.abe@gemcook.com>
2020-08-05 08:30:21 +0900

    Added Route.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import Signin from './components/Signin';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const routing = (
+  <BrowserRouter>
+    <CookiesProvider>
+      <Route exact path='/' component={Signin} />
+      <Route exact path='/home' component={App} />
+    </CookiesProvider>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routing, document.querySelector('#root'));
